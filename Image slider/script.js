f623315167e036173b616df8e436dd092d5a8938
Vadim Macovei <burebistamacovei@gmail.com>
2022-06-16 16:48:48 +0300

    Correct small errors
    Add last link to list in readme file
    Change files for slider project

--- Image slider/script.js
+++ Image slider/script.js
@@ -1,27 +1,49 @@
-$(document).ready(function() {
-   $('#next').on('click', function() {
-       var currentImg = $('.active');
-       var nextImg = currentImg.next();
+var pics = [
+  "https://picsum.photos/id/128/700/500",
+  "https://picsum.photos/id/1057/700/500",
+  "https://picsum.photos/id/1043/700/500",
+  "https://picsum.photos/id/1050/700/500",
+  "https://picsum.photos/id/1020/700/500",
+  "https://picsum.photos/id/651/700/500",
+  "https://picsum.photos/id/634/700/500",
+  "https://picsum.photos/id/599/700/500",
+  "https://picsum.photos/id/575/700/500",
+  "https://picsum.photos/id/574/700/500",
+  "https://picsum.photos/id/568/700/500",
+  "https://picsum.photos/id/566/700/500",
+  "https://picsum.photos/id/558/700/500",
+  "https://picsum.photos/id/507/700/500",
+  "https://picsum.photos/id/501/700/500",
+  "https://picsum.photos/id/353/700/500",
+  "https://picsum.photos/id/323/700/500",
+  "https://picsum.photos/id/287/700/500",
+  "https://picsum.photos/id/730/700/500",
+  "https://picsum.photos/id/779/700/500",
+];
 
-       if (nextImg.length) {
-           currentImg.removeClass('active').css('z-index', -10);
-           nextImg.addClass('active').css('z-index', 10);
-       }
-       console.log(currentImg)
-   });
+var prev = document.querySelector("#prev");
+var next = document.querySelector("#next");
+var img = document.querySelector("img");
+var counter = 0;
 
-   $('#prev').on('click', function() {
-       var currentImg = $('.active');
-       var prevImg = currentImg.prev();
+next.addEventListener("click", function () {
+  counter = counter + 1;
 
-       if (prevImg.length) {
-           currentImg.removeClass('active').css('z-index', -10);
-           prevImg.addClass('active').css('z-index', 10);
-       }
-       console.log(currentImg)
-   });
+  if (counter > pics.length - 1) {
+    counter = 0;
+  }
 
-   if (condition) {
-       
-   }
+  img.src = pics[counter];
+  console.log(counter);
+});
+
+prev.addEventListener("click", function () {
+  counter = counter - 1;
+
+  if (counter < 0) {
+    counter = pics.length - 1;
+  }
+
+  img.src = pics[counter];
+  console.log(counter);
 });
--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 1. [Chess Board](https://trusting-bhabha-c67443.netlify.app/chess-board/) + double margins + for + forEach + addEventListener
 2. [Image slider](https://trusting-bhabha-c67443.netlify.app/image%20slider/) + if => let's see how it works
 3. [Accordion](https://trusting-bhabha-c67443.netlify.app/accordion/) + for + classList + toggle + this
-4. Day and Night + Animation
+4. [Day and Night](https://trusting-bhabha-c67443.netlify.app/animation%20day%20and%20night/) + Animation
 5. [4 squares](https://trusting-bhabha-c67443.netlify.app/4%20squares%20animation/index.html) & [4 squares 2](https://trusting-bhabha-c67443.netlify.app/4%20squares%20animation%202/index.html) + Animation
 6. [Simplest Light and Dark Theme](https://trusting-bhabha-c67443.netlify.app/simplest%20light%20and%20dark%20theme/) + '.checkbox:checked + .label'
 7. [Light Dark Theme](https://trusting-bhabha-c67443.netlify.app/light%20dark%20theme%20toggle/) + CSS Var + Grid + SASS
