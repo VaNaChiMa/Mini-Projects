296119c9e551b2e2e9a21c2eeed97f2a31747082
Vadim Macovei <burebistamacovei@gmail.com>
2022-07-30 15:00:34 +0300

    Update Clocks project
    Add/fix JS part

--- Clocks/app.js
+++ Clocks/app.js
@@ -1,53 +1,67 @@
-const clock = document.querySelector(".clock");
-const chisinau = document.querySelector("#chisinau");
-const newYork = document.querySelector("#new-york");
-chisinau.addEventListener("click", function () {});
-
-newYork.addEventListener("click", function () {
-  startTime();
-});
-
-function startTime() {
-  const now = new Date();
-
-  let h = now.getHours();
-  let m = now.getMinutes();
-  let s = now.getSeconds();
-
-  h = checkNumber(h);
-  m = checkNumber(m);
-  s = checkNumber(s);
-
-  document.querySelector(".clock").innerHTML = `${h}:${m}:${s}`;
-
-  setTimeout(startTime, 1000);
-}
-
-// add zero in front of numbers < 10
-function checkNumber(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
-
-startTime();
-
 const buttons = document.querySelectorAll("button");
+const clocks = document.querySelectorAll(".clock");
+const chisinau = document.getElementById("chisinau");
+const newYork = document.getElementById("new-york");
+const tokyo = document.getElementById("tokyo");
+const capeTown = document.getElementById("cape-town");
+const buenosAires = document.getElementById("buenos-aires");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", function () {
-    removeActiveClasses();
-    if (button.className === "") {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
+const timeZones = [
+  "Europe/Chisinau",
+  "America/New_York",
+  "Asia/Tokyo",
+  "Africa/Cairo",
+  "America/Argentina/Buenos_Aires",
+];
+
+for (let i = 0; i < clocks.length; i++) {
+  setInterval(function tick() {
+    let time = new Date();
+    time = time.toLocaleTimeString("en-US", { timeZone: timeZones[i] });
+    clocks[i].innerText = time;
+    // console.log(time);
+  }, 1000);
+}
+
+function addActiveClasses() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      removeActiveClasses();
+      if (button.className === "") {
+        button.classList.add("active");
+        if (this.textContent === "New-York") {
+          removeShowClasses();
+          newYork.classList.add("show");
+        } else if (this.textContent === "Tokyo") {
+          removeShowClasses();
+          tokyo.classList.add("show");
+        } else if (this.textContent === "Cape Town") {
+          removeShowClasses();
+          capeTown.classList.add("show");
+        } else if (this.textContent === "Buenos Aires") {
+          removeShowClasses();
+          buenosAires.classList.add("show");
+        } else {
+          removeShowClasses();
+          chisinau.classList.add("show");
+        }
+      } else {
+        button.classList.remove("active");
+      }
+      // console.log(this.textContent);
+    });
   });
-});
+}
 
 function removeActiveClasses() {
   buttons.forEach((button) => {
     button.classList.remove("active");
   });
 }
+function removeShowClasses() {
+  clocks.forEach((clock) => {
+    clock.classList.remove("show");
+  });
+}
+
+addActiveClasses();
