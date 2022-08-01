4eecd6f866f63b5e849b6b394923788fec1534ef
Vadim Macovei <burebistamacovei@gmail.com>
2022-08-01 20:45:54 +0300

    Update Color Game
    Improve UI,
    Visibility of h1 and squares

--- Color Game/script.js
+++ Color Game/script.js
@@ -1,65 +1,54 @@
+const myBtn = document.querySelector("#myBtn");
+const myHeading = document.querySelector("h1");
+const squaresContainer = document.querySelector(".squarsContainer");
+const square = document.querySelectorAll(".square");
 
-const myBtn = document.querySelector('#myBtn');
-const myHeading = document.querySelector('h1');
-const container = document.querySelector('.container');
-// const squares = document.querySelectorAll('.square');
 let count = 0;
 let squaresCount = [];
 let colors = [];
 
+myBtn.addEventListener("click", function () {
+  createNewColor();
 
-myBtn.addEventListener('click', function() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r}, ${g}, ${b})`;
-    document.body.style.backgroundColor = newColor;
-    myHeading.innerText = newColor;
+  createSquare();
 
-    function returnColor() {
-        return newColor;
+  for (let i = 0; i < squaresCount.length; i++) {
+    for (let a = 0; a < colors.length; a++) {
+      squaresCount[a].style.backgroundColor = colors[a];
     }
-    
-    colors.push(returnColor());
-    console.log(colors);
-
-    createSquare();
-
-    for (let i = 0; i < squaresCount.length; i++) {
-        for (let a = 0; a < colors.length; a++) {
-            squaresCount[a].style.backgroundColor = colors[a];
-        }
-    }
-
-    if(r + g + b <= 150) {
-        myHeading.style.color = "#fff"
-     } else {
-         myHeading.style.color = "#000"
-     }
-
+  }
 });
 
 function createSquare() {
-    const squareColor = document.createElement("div");
-    squareColor.innerHTML = "COLOR";
-    container.appendChild(squareColor);
-    squareColor.className = "square";
-    // squareColor.style.backgroundColor = '#fff';
-    
-    count+=1;
-    squareColor.innerHTML = count;
+  const squareColor = document.createElement("div");
+  const squareColorNum = document.createElement("p");
 
-    squaresCount.push(squareColor);
-    console.log(squaresCount);
+  squaresContainer.appendChild(squareColor);
+  squareColor.appendChild(squareColorNum);
+  squareColor.className = "square";
 
-    squaresCount.forEach(square => {
-        square.addEventListener('click', function() {
-         document.body.style.backgroundColor = this.style.backgroundColor;
-         myHeading.innerText = this.style.backgroundColor;
-        })
-    })
+  count += 1;
+  squareColorNum.innerHTML = count;
+
+  squaresCount.push(squareColor);
+
+  squaresCount.forEach((square) => {
+    square.addEventListener("click", function () {
+      document.body.style.backgroundColor = this.style.backgroundColor;
+      myHeading.innerText = `Color: ${this.style.backgroundColor}`;
+    });
+  });
 }
 
+function createNewColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  const newColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = newColor;
+  myHeading.innerText = `Color: ${newColor}`;
 
+  colors.push(newColor);
 
-
+  return newColor;
+}
